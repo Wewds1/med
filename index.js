c853395ff3797865dc02ba5669c307f4706de24c
allend <daveallenborja@gmail.com>
2022-03-22 22:35:06 +0800

    make it better

--- index.js
+++ index.js
@@ -7,7 +7,40 @@ const io = socket(server);
 const path = require('path');
 const cors = require('cors')
 const PORT = process.env.PORT || 443;
+const whitelist = ['http://localhost:3000', 'http://localhost:443', 'https://heroku123-app.herokuapp.com']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
+app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get("/", (req, res) => {
+    res.send("Server is running");
+  });
+  
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+  
+
+
+
+=======
 if (process.env.NODE_ENV === "production"){
   app.use(express.static('build'));
   app.get('*',(req,res) => {
@@ -15,6 +48,7 @@ if (process.env.NODE_ENV === "production"){
   })
 }
 
+>>>>>>> 1ce9e8965c40ca627b8f2c8ae9b09c635a4eb39b
 const users = {};
 
 const socketToRoom = {};
